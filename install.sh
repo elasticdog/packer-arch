582b8385557b85e657579292cbc81119aedd11c3
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-08-22 13:02:37 -0700

    Fix configuration script permissions

--- install.sh
+++ install.sh
@@ -4,15 +4,12 @@ DISK='/dev/sda'
 FQDN='vagrant-arch.vagrantup.com'
 KEYMAP='us'
 LANGUAGE='en_US.UTF-8'
-PASSWORD=$(/usr/bin/openssl passwd -crypt "vagrant"})
+PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
 TIMEZONE='UTC'
 
 ROOT_PARTITION="${DISK}1"
-
-# add_config appends lines to a file which will be run in the final chroot system
-add_config() {
-  echo "${1}" >> /mnt/usr/local/bin/arch-config.sh
-}
+TARGET_DIR='/mnt'
+CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 
 echo "==> clearing partition table on ${DISK}"
 /usr/bin/sgdisk --zap ${DISK}
@@ -30,19 +27,24 @@ echo "==> setting ${DISK} bootable"
 echo '==> creating /root filesystem (ext4)'
 /usr/bin/mkfs.ext4 -F -m 0 -q -L root ${ROOT_PARTITION}
 
-echo "==> mounting ${ROOT_PARTITION} to /mnt"
-/usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} /mnt
+echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
+/usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} ${TARGET_DIR}
 
 echo '==> bootstrapping the base installation'
-/usr/bin/pacstrap /mnt base base-devel
-/usr/bin/arch-chroot /mnt pacman -S --noconfirm syslinux gptfdisk
-/usr/bin/arch-chroot /mnt syslinux-install_update -i -a -m
-/usr/bin/sed -i 's/sda3/sda1/' /mnt/boot/syslinux/syslinux.cfg
+/usr/bin/pacstrap ${TARGET_DIR} base base-devel
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm syslinux gptfdisk
+/usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
+/usr/bin/sed -i 's/sda3/sda1/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
 
 echo '==> generating the filesystem table'
-/usr/bin/genfstab -p /mnt >> /mnt/etc/fstab
+/usr/bin/genfstab -p ${TARGET_DIR} >> "${TARGET_DIR}/etc/fstab"
 
 echo '==> generating the system configuration script'
+/usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
+
+add_config() {
+  echo "${1}" >> "${TARGET_DIR}${CONFIG_SCRIPT}"
+}
 
 add_config "echo \"${FQDN}\" > /etc/hostname"
 add_config "/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime"
@@ -60,5 +62,5 @@ add_config '/usr/bin/systemctl enable dhcpcd@eth0'
 add_config '/usr/bin/pacman -Scc --noconfirm'
 
 echo '==> entering chroot and configuring system'
-/usr/bin/arch-chroot /mnt /usr/local/bin/arch-config.sh
-/usr/bin/arch-chroot /mnt rm /usr/local/bin/arch-config.sh
+/usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
+rm "${TARGET_DIR}${CONFIG_SCRIPT}"
