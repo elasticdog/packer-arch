744a473cb84c289e7634fefe72afdb985d600f4c
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-08-22 13:31:13 -0700

    Simplify and add umount/reboot at the end

--- install.sh
+++ install.sh
@@ -4,18 +4,19 @@ DISK='/dev/sda'
 FQDN='vagrant-arch.vagrantup.com'
 KEYMAP='us'
 LANGUAGE='en_US.UTF-8'
+NIC_DEVICE='ens33'
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
 TIMEZONE='UTC'
 
+CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 ROOT_PARTITION="${DISK}1"
 TARGET_DIR='/mnt'
-CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 
 echo "==> clearing partition table on ${DISK}"
 /usr/bin/sgdisk --zap ${DISK}
 
 echo "==> destroying magic strings and signatures on ${DISK}"
-/usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048 >/dev/null 2>&1
+/usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048
 /usr/bin/wipefs --all ${DISK}
 
 echo "==> creating /root partition on ${DISK}"
@@ -32,7 +33,7 @@ echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 
 echo '==> bootstrapping the base installation'
 /usr/bin/pacstrap ${TARGET_DIR} base base-devel
-/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm syslinux gptfdisk
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux
 /usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
 /usr/bin/sed -i 's/sda3/sda1/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
 
@@ -52,15 +53,15 @@ add_config "echo "KEYMAP=${KEYMAP}" > /etc/vconsole.conf"
 add_config "/usr/bin/sed -i \"s/#${LANGUAGE}/${LANGUAGE}/\" /etc/locale.gen"
 add_config '/usr/bin/locale-gen'
 add_config '/usr/bin/mkinitcpio -p linux'
-add_config '/usr/bin/hwclock --systohc --utc'
 add_config "/usr/bin/usermod --password ${PASSWORD} root"
 add_config "/usr/bin/useradd --password ${PASSWORD} --comment \"Vagrant User\" --create-home --gid users vagrant"
 add_config "echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/10_vagrant"
-add_config '/usr/bin/systemctl start sshdgenkeys.service'
 add_config '/usr/bin/systemctl enable sshd.service'
-add_config '/usr/bin/systemctl enable dhcpcd@eth0'
+add_config "/usr/bin/systemctl enable dhcpcd@${NIC_DEVICE}"
 add_config '/usr/bin/pacman -Scc --noconfirm'
 
 echo '==> entering chroot and configuring system'
 /usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
 rm "${TARGET_DIR}${CONFIG_SCRIPT}"
+/usr/bin/umount ${TARGET_DIR}
+/usr/bin/systemctl reboot
