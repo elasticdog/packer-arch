e6d4b5346280d518486c4da6649da6f065b0a94d
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-08-23 22:14:31 -0700

    Remove add_config function and just use a heredoc

--- install.sh
+++ install.sh
@@ -43,35 +43,33 @@ echo '==> generating the filesystem table'
 echo '==> generating the system configuration script'
 /usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
 
-add_config() {
-  echo "${1}" >> "${TARGET_DIR}${CONFIG_SCRIPT}"
-}
+cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
+	echo '${FQDN}' > /etc/hostname
+	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
+	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
+	/usr/bin/locale-gen
+	/usr/bin/mkinitcpio -p linux
+	/usr/bin/usermod --password ${PASSWORD} root
+	# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
+	/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
+	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
+	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+	/usr/bin/systemctl enable sshd.service
 
-add_config "echo '${FQDN}' > /etc/hostname"
-add_config "/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime"
-add_config "echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf"
-add_config "/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen"
-add_config '/usr/bin/locale-gen'
-add_config '/usr/bin/mkinitcpio -p linux'
-add_config "/usr/bin/usermod --password ${PASSWORD} root"
-# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
-add_config '/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules'
-add_config "/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'"
-add_config "/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config"
-add_config '/usr/bin/systemctl enable sshd.service'
+	# Vagrant-specific configuration
+	/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --gid users vagrant
+	echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
+	echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
+	/usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh
+	/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+	/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
+	/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
 
-# Vagrant-specific configuration
-add_config "/usr/bin/useradd --password ${PASSWORD} --comment \"Vagrant User\" --create-home --gid users vagrant"
-add_config "echo 'Defaults env_keep += \"SSH_AUTH_SOCK\"' > /etc/sudoers.d/10_vagrant"
-add_config "echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant"
-add_config '/usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh'
-add_config '/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-add_config '/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys'
-add_config '/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys'
-
-# clean up
-add_config '/usr/bin/pacman -Rcns --noconfirm gptfdisk'
-add_config '/usr/bin/pacman -Scc --noconfirm'
+	# clean up
+	/usr/bin/pacman -Rcns --noconfirm gptfdisk
+	/usr/bin/pacman -Scc --noconfirm
+EOF
 
 echo '==> entering chroot and configuring system'
 /usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
