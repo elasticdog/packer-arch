c553f3c26ebbc21596aa18ebfb6f8c041d29e3a9
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-09-26 16:17:18 -0700

    Remove redundant dkms add vboxguest command

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -62,7 +62,6 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
 	guest_version=\$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)
 	kernel_version="\$(/usr/bin/pacman -Q linux | awk '{ print \$2 }')-ARCH"
-	/usr/bin/dkms add "vboxguest/\${guest_version}"
 	/usr/bin/dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
 	/usr/bin/systemctl enable dkms.service
 	/usr/bin/systemctl enable vboxservice.service
