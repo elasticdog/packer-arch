6f747f94258b35f74c4fccb11c066f2933de5dd7
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-09-26 11:29:28 -0700

    Add a workaround for the shutdown race condition
    http://comments.gmane.org/gmane.linux.arch.general/48739
    
    Basically systemd kills the networking before it kills ssh sessions, and
    Packer will try and wait for a 0 exit code after the shutdown command to
    confirm that it worked. Things happen too quickly with systemd, and so
    Packer complains when gracefully halting the machine that:
    
        "Build 'vmware' errored: Shutdown command has non-zero exit status."
    
    This workaround just adds a standard systemd timer to the poweroff
    command so it has a one second delay, which is long enough for the
    0 exit code to return and signal a successful graceful halt.

--- install.sh
+++ install.sh
@@ -66,6 +66,8 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 	/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
 	/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
+	# workaround for shutdown race condition: http://comments.gmane.org/gmane.linux.arch.general/48739
+	/usr/bin/curl --output /etc/systemd/system/poweroff.timer https://raw.github.com/elasticdog/packer-arch/master/poweroff.timer
 
 	# clean up
 	/usr/bin/pacman -Rcns --noconfirm gptfdisk
