abcec6125d292d28fa0186168270e9ae10b63604
Aaron Bull Schaefer <aaron@elasticdog.com>
2013-09-26 15:41:01 -0700

    Escape DKMS variables in installation script
    Otherwise these will get expanded based on values from the installer
    ISO and not from the chroot'ed system.

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -60,10 +60,10 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	# VirtualBox Guest Additions
 	/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms
 	echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
-	guest_version=$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print $2 }' | cut -d'-' -f1)
-	kernel_version="$(/usr/bin/pacman -Q linux | awk '{ print $2 }')-ARCH"
-	/usr/bin/dkms add "vboxguest/${guest_version}"
-	/usr/bin/dkms install "vboxguest/${guest_version}" -k "${kernel_version}/x86_64"
+	guest_version=\$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)
+	kernel_version="\$(/usr/bin/pacman -Q linux | awk '{ print \$2 }')-ARCH"
+	/usr/bin/dkms add "vboxguest/\${guest_version}"
+	/usr/bin/dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
 	/usr/bin/systemctl enable dkms.service
 
 	# Vagrant-specific configuration
