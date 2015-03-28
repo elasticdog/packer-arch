519697c805b136080a11722a28baf67f3e02078b
viq <vicviq@gmail.com>
2015-03-28 23:03:11 +0100

    Enable support for NFS shared folders
    Add necessary bits to allow mounting shared folders via NFS

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -58,13 +58,14 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	/usr/bin/systemctl enable sshd.service
 
 	# VirtualBox Guest Additions
-	/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms
+	/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms nfs-utils
 	echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
 	guest_version=\$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)
 	kernel_version="\$(/usr/bin/pacman -Q linux | awk '{ print \$2 }')-ARCH"
 	/usr/bin/dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
 	/usr/bin/systemctl enable dkms.service
 	/usr/bin/systemctl enable vboxservice.service
+	/usr/bin/systemctl enable rpcbind.service
 
 	# Vagrant-specific configuration
 	/usr/bin/groupadd vagrant
