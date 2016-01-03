f616487af3fe805b72932f379f7e29ffc375985d
dragon788 <dragon788@users.noreply.github.com>
2016-01-03 00:14:50 -0600

    Added parallels tools in a better way

--- scripts/parallels.sh
+++ scripts/parallels.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-echo '==> Include parallels tools'
-/usr/bin/install --mode=0755 parallels_tools.sh "${TARGET_DIR}/parallels_tools.sh"
+echo '==> Install parallels tools'
+# Install parallels tools (https://wiki.archlinux.org/index.php/Parallels)
+mount /dev/sr1 /mnt
+ln -sf /usr/lib/systemd/scripts/ /etc/init.d
+export def_sysconfdir=/etc/init.d
+touch /etc/X11/xorg.conf
+pacman -S --noconfirm python2 linux-headers
+ln -sf /usr/bin/python2 /usr/local/bin/python
+/mnt/install --install-unattended
+
+# Cleanup after parallels tools installation
+umount /dev/sr1
+/usr/bin/pacman -Rcns --noconfirm python2 linux-headers
+rm -f /etc/init.d
+rm -f /etc/X11/xorg.conf
+rm -f /usr/local/bin/python
 
 echo '==> installation complete!'
 /usr/bin/sleep 3
