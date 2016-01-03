f253adefbb95be21994ba8c198873a34e8c11868
dragon788 <dragon788@users.noreply.github.com>
2016-01-02 23:21:38 -0600

    Baseline scripts for installing common arch components

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -5,32 +5,11 @@ SSH_USERNAME=${SSH_USERNAME:-vagrant}
 if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     echo "==> Installing VMware Tools"
     # Assuming the following packages are installed
-    # pacman -S --needed --no-confirm linux-headers base base-dev perl
-
+    #pacman -Syu --needed --noconfirm
+    pacman -S --needed --noconfirm wget
     cd /tmp
-    mkdir -p /mnt/cdrom
-    mount -o loop /home/${SSH_USERNAME}/linux.iso /mnt/cdrom
-
-    VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
-    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
-    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
-    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
-    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
-    echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
-    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}" 
-
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
-    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
-        /tmp/vmware-tools-distrib/vmware-install.pl -d
-    else
-        /tmp/vmware-tools-distrib/vmware-install.pl -f
-    fi
-
-    rm /home/${SSH_USERNAME}/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
-    rm -rf /tmp/VMwareTools-*
+    wget https://raw.githubusercontent.com/dragon788/vmware-tools-patches/master/patched-open-vm-tools.sh
+    ./patched-open-vm-tools.sh
 
     VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
     echo "==> Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}" 
