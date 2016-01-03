ec70b6ff1be443a2ca4ef12db2b150934cdca75e
dragon788 <dragon788@users.noreply.github.com>
2016-01-02 23:42:31 -0600

    Better vmware/virtualbox separation

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,11 +5,6 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 # Make sure udev does not block our network - http://6.ptmc.org/?p=164
 echo "==> Cleaning up udev rules"
 rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
-
-
-# Add delay to prevent "vagrant reload" from failing
-echo "pre-up sleep 2" >> /etc/network/interfaces
 
 echo "==> Cleaning up tmp"
 rm -rf /tmp/*
--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -7,8 +7,15 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     # Assuming the following packages are installed
     #pacman -Syu --needed --noconfirm
     pacman -S --needed --noconfirm wget
-    cd /tmp
+    # System /tmp isn't large enough for this operation
+    mkdir -p /home/${SSH_USERNAME}/tmp
+    cd /home/${SSH_USERNAME}/tmp
+
+    # This is a fix for Arch not having SysVinit dirs
+    for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
+
     wget https://raw.githubusercontent.com/dragon788/vmware-tools-patches/master/patched-open-vm-tools.sh
+    chmod +x patched-open-vm-tools.sh
     ./patched-open-vm-tools.sh
 
     VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
