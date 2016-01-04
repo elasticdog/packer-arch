824e9dc720c19591f04993df855afc40becf7546
dragon788 <dragon788@users.noreply.github.com>
2016-01-03 19:29:57 -0600

    Removed unnecessary open-vm-tools code

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -7,7 +7,7 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     # Assuming the following packages are installed
     #pacman -Syu --needed --noconfirm
     # These are arch specific changes, so not putting in the patches repo
-    pacman -S --needed --noconfirm wget abs
+    pacman -S --needed --noconfirm wget 
     # System /tmp isn't large enough for this operation
     mkdir -p /home/${SSH_USERNAME}/tmp
     cd /home/${SSH_USERNAME}/tmp
@@ -17,17 +17,10 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
 
     wget https://raw.githubusercontent.com/dragon788/vmware-tools-patches/master/patched-open-vm-tools.sh
     chmod +x patched-open-vm-tools.sh
+    # Running the compile.sh in patched-open-vm-tools above should install and activate the 
+    # vmware tools services we need for shared folders
     ./patched-open-vm-tools.sh
 
     VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
     echo "==> Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}" 
-    # These are arch specific changes, so not putting in the patches repo
-    # I'm not convinced I need these service definitions, as vmtoolsd is separate
-    #abs community/open-vm-tools
-    #cp /var/abs/community/open-vm-tools/vmware-* /usr/lib/systemd/system
-    #systemctl enable vmware-vmblock-fuse.service
-    # Running the compile.sh in patched-open-vm-tools above should install and activate the 
-    # vmware tools services we need for shared folders
-
-
 fi
