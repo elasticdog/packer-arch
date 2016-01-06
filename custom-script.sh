e96c4f1cec44a49544e3fd8dcc16db8add0883df
dragon788 <dragon788@users.noreply.github.com>
2016-01-05 22:19:12 -0600

    Removing unused scripts

--- custom-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-# Sample custom configuration script - add your own commands here
-# to add some additional commands for your environment
-#
-# For example:
-# pacman -S --needed --no-confirm curl wget git tmux firefox xvfb
