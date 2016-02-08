d5adbc3f104dfd1af5aaf2602f7f95324d31b575
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-02-07 22:38:39 -0800

    Remove duplication in installation scripts
    Instead of having mostly duplicate scripts for the entire installation
    process, we can split them up into the base installation steps and then
    run shell provisioners for anything specific to each of the types after
    the machine has rebooted.
    
    This structure will mean less chance of error when making changes to
    machines, and will also allow for expansion of the scripts to be run at
    the end, such as adding a minimization script to remove history and
    clear out space on the drive before compression.

--- scripts/install-parallels.sh
+++ scripts/install-parallels.sh
@@ -1,4 +1,7 @@
-# Install parallels tools (https://wiki.archlinux.org/index.php/Parallels)
+#!/usr/bin/bash -x
+
+# Parallels Tools
+# https://wiki.archlinux.org/index.php/Parallels
 mount /dev/sr1 /mnt
 ln -sf /usr/lib/systemd/scripts/ /etc/init.d
 export def_sysconfdir=/etc/init.d
@@ -7,7 +10,7 @@ pacman -S --noconfirm python2 linux-headers
 ln -sf /usr/bin/python2 /usr/local/bin/python
 /mnt/install --install-unattended
 
-# Cleanup after parallels tools installation
+# clean up
 umount /dev/sr1
 /usr/bin/pacman -Rcns --noconfirm python2 linux-headers
 rm -f /etc/init.d
