fd57a360b7ee455a879a8b8dc68072409b8c2a2a
Mike Appleby <mike@app.leby.org>
2016-02-09 20:50:05 -0600

    Add a cleanup script.
    Do a final clean of the pacman cache, then write zeros to disk and
    unlink the file.  Packer will compact the disk as part of the build,
    resulting in a smaller final image.

---
+++ scripts/cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash -x
+
+# Clean the pacman cache.
+/usr/bin/yes | /usr/bin/pacman -Scc
+
+# Write zeros to improve virtual disk compaction.
+zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
+/usr/bin/dd if=/dev/zero of=${zerofile} bs=1M
+/usr/bin/rm -f ${zerofile}
+/usr/bin/sync
