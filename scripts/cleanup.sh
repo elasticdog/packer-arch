6c589e733a2c4522c24d494e6b16e640c94e587a
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-02-11 08:00:09 -0800

    Remove the conditional on cleanup.sh
    This should be desireable to run on any of the builders.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,6 +5,6 @@
 
 # Write zeros to improve virtual disk compaction.
 zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
-/usr/bin/dd if=/dev/zero of=${zerofile} bs=1M
-/usr/bin/rm -f ${zerofile}
+/usr/bin/dd if=/dev/zero of="$zerofile" bs=1M
+/usr/bin/rm -f "$zerofile"
 /usr/bin/sync
