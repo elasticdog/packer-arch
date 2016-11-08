8f208034ed453feb4c70542cb88a96bb242d94c4
Tom Swartz <tom.swartz07@gmail.com>
2016-11-07 19:58:25 -0500

    Rename SSH Setup Script
    Change name of SSH setup script to reflect actions
    Remove unnecessary SSH key setup in pre-install environment

--- srv/enable-ssh.sh
+++ srv/enable-ssh.sh
@@ -8,9 +8,4 @@ echo "==> Enabling SSH"
 echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
 /usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
-/usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh
-/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
-/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
-/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
 /usr/bin/systemctl start sshd.service
-
