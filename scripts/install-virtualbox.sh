e4bcd86a634a8118c2335e5243dac7f8a17cba2f
Louis Bellet <lbellet@heliostech.fr>
2017-01-12 22:52:11 +0200

    Fixing Guest Virtualbox tools
    Archlinux wiki suggest for linux kernel using virtualbox-host-modules-arch

--- scripts/install-virtualbox.sh
+++ scripts/install-virtualbox.sh
@@ -2,15 +2,11 @@
 
 # VirtualBox Guest Additions
 # https://wiki.archlinux.org/index.php/VirtualBox
-/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms nfs-utils
+/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-modules-arch nfs-utils
 echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
-guest_version=$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print $2 }' | cut -d'-' -f1)
-kernel_version="$(/usr/bin/pacman -Q linux | awk '{ print $2 }')-ARCH"
-/usr/bin/dkms install "vboxguest/${guest_version}" -k "${kernel_version}/x86_64"
-/usr/bin/systemctl enable dkms.service
+
 /usr/bin/systemctl enable vboxservice.service
 /usr/bin/systemctl enable rpcbind.service
 
 # Add groups for VirtualBox folder sharing
-/usr/bin/groupadd vagrant
 /usr/bin/usermod --append --groups vagrant,vboxsf vagrant
