cfb25be9d6ac60e1c53b1f0df1c80a7576aa408c
Christian Kotte <christian.kotte@gmx.de>
2020-07-12 22:27:25 +0200

    Improve script output
    The '==>' prefix is used by pacstrap, pacman, and mkinitcpio. A new
    prefix is needed to better differentiate the output of the script from
    the output of the programs mentioned above.
    Also adding more output to the different steps helps during troubleshooting.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash -x
 
 # Clean the pacman cache.
-/usr/bin/yes | /usr/bin/pacman -Scc
+echo ">>>> cleanup.sh: Cleaning pacman cache.."
+/usr/bin/pacman -Scc --noconfirm
 
 # Write zeros to improve virtual disk compaction.
+echo ">>>> cleanup.sh: Writing zeros to improve virtual disk compaction.."
 zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
 /usr/bin/dd if=/dev/zero of="$zerofile" bs=1M
 /usr/bin/rm -f "$zerofile"
--- scripts/install-vmware.sh
+++ scripts/install-vmware.sh
@@ -3,7 +3,11 @@
 # Open VM Tools
 # https://wiki.archlinux.org/index.php/VMware
 # https://wiki.archlinux.org/index.php/VMware/Installing_Arch_as_a_guest
+echo ">>>> install-virtualbox.sh: Installing Open-VM-Tools and NFS utilities.."
 /usr/bin/pacman -S --noconfirm linux-headers open-vm-tools nfs-utils
 
+echo ">>>> install-virtualbox.sh: Enabling Open-VM-Tools service.."
 /usr/bin/systemctl enable vmtoolsd.service
+
+echo ">>>> install-virtualbox.sh: Enabling RPC Bind service.."
 /usr/bin/systemctl enable rpcbind.service
